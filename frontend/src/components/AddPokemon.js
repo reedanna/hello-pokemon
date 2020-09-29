030b676d4912dbbc0dd74e41e92621812f0cf091
reedanna <reedanna@mail.com>
2020-09-29 11:03:42 -0500

    Created basic component layout

---
+++ frontend/src/components/AddPokemon.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import AllPokemon from './AllPokemon.js'
+
+export default class AddPokemon extends Component {
+    render() {
+      return (
+        <div>
+          Add Pokemon
+        </div>
+      )
+    }
+  }
---
+++ frontend/src/components/Pokemon.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Pokemon extends Component {
+    render() {
+      return (
+        <div>
+          Pokemon
+        </div>
+      )
+    }
+  }
