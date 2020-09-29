c7b749db277afeac2c3d2d886a7071f872608fe5
reedanna <reedanna@mail.com>
2020-09-29 16:36:45 -0500

    Login and signup now available

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Index from './components/Index.js';
+import Header from './components/Header.js';
 import Userpage from './components/Userpage';
 import Pokedex from './components/Pokedex';
 
@@ -8,9 +9,7 @@ import Pokedex from './components/Pokedex';
 function App() {
   return (
     <div className="grid-container"> 
-    <div className="header">
-      <h1 className="hellopokemons"><span className="first">"HELLO,</span><span className="second"> POKEMONS"</span></h1>
-    </div>
+    <Header />
     <Index />
     </div>
   );
