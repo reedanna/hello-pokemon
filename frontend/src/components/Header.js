f3c8c05ca0ac1b78f964ee73d1afecc4ae140353
reedanna <reedanna@mail.com>
2020-09-30 16:23:04 -0500

    added login and signup routes, and fixed Pokemon order in Pokedex

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -4,7 +4,7 @@ export default class Header extends Component {
   render() {
     return (
       <div className="header">
-        <h1 className="hellopokemons"><span className="first">"HELLO,</span><span className="second"> POKEMONS"</span></h1>
+        <h1 className="hellopokemons"><span className="first">"HELLO,</span><span className="second"> POKEMON"</span></h1>
       </div>
     )
   }
