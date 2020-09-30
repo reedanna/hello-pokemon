1091a05de5b3b099ebcb9aa561979fabff5fea22
sohyun <sohyunc1990@gmail.com>
2020-09-30 09:00:42 -0500

    worked on pokedex and allpokes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="header">
       <h1 className="hellopokemons"><span className="first">"HELLO,</span><span className="second"> POKEMONS"</span></h1>
     </div>
-    <Index />
+    <Main />
     </div>
   );
 }
