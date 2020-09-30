b249e417d7ab92027696dfeaa739344df680292e
reedanna <reedanna@mail.com>
2020-09-30 09:57:37 -0500

    Did some organizing, will do more in my own branch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,17 +2,20 @@ import React from 'react';
 import './App.css';
 import Index from './components/Index.js';
 import Header from './components/Header.js';
+import Main from './components/Main.js';
 import Userpage from './components/Userpage';
 import Pokedex from './components/Pokedex';
 
 
 function App() {
+
   return (
-    <div className="grid-container"> 
-    <Header />
-    <Index />
+    <div className="grid-container">
+      <Header />
+      <Main />
     </div>
   );
+
 }
 
 export default App;
--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,37 +1,41 @@
 import React from 'react'
 import Pokedex from './Pokedex.js';
 
-class Main extends React.Component{
-    constructor(){
+class Main extends React.Component {
+    constructor() {
         super()
-        this.state={
+        this.state = {
             pokemons: [],
             myteam: false,
             pokedex: false
         }
     }
-componentDidMount(){
-    fetch('http://localhost:3001/pokemons').then(res=>res.json()).then(json=>this.setState({pokemons: json}))
-}
+    componentDidMount() {
+        fetch('http://localhost:3000/pokemons')
+            .then(res => res.json())
+            .then(json => {
+                this.setState({ pokemons: json })
+            })
+    }
 
-    render(){
-        return(
+    render() {
+        return (
             <>
-            {this.state.myteam? 
-            "myteam"
-            : null}
-            {this.state.pokedex?
-            <Pokedex pokemons={this.state.pokemons}/>
-            : null}
-            <div className="c">
-                <div className="pokeball" onClick={()=>this.setState({myteam: true, pokedex: false})}>My Team</div>
-            </div>
-            <div className="d">
-                <div className="pokeball" onClick={()=>this.setState({pokedex: true, myteam: false})}>Pokedex</div>
-            </div>
-            <div className="f">
-                <div className="pokeball">Logout</div>
-            </div>
+                {this.state.myteam ?
+                    "myteam"
+                    : null}
+                {this.state.pokedex ?
+                    <Pokedex pokemons={this.state.pokemons} />
+                    : null}
+                <div className="c">
+                    <div className="pokeball" onClick={() => this.setState({ myteam: true, pokedex: false })}>My Team</div>
+                </div>
+                <div className="d">
+                    <div className="pokeball" onClick={() => this.setState({ pokedex: true, myteam: false })}>Pokedex</div>
+                </div>
+                <div className="f">
+                    <div className="pokeball">Logout</div>
+                </div>
             </>
         )
     }
