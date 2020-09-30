149132f6f18a229c660116485f3fbb13c3df732f
reedanna <reedanna@mail.com>
2020-09-30 16:22:14 -0500

    Added login and signup routes, fixed Pokemon order in Pokedex

--- frontend/src/components/Index.js
+++ frontend/src/components/Index.js
@@ -1,74 +1,27 @@
 import React from 'react'
+import Login from './Login.js'
+import Signup from './Signup.js'
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 
 class Index extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            loginForm: false,
-            signupForm: false
-        }
-    }
-
-    login = (e) => {
-        fetch('http://localhost:3000/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                name: e.target.parentElement.username.value,
-                password: e.target.parentElement.password.value
-            })
-        })
-            .then(response => response.json())
-            .then(data => {
-                this.props.setCurrentUser(data.user)
-            })
-    }
-
-    signup = (e) => {
-        fetch('http://localhost:3000/users', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                name: e.target.parentElement.username.value,
-                password: e.target.parentElement.password.value
-            })
-        })
-    }
-
     render() {
         return (
-            <>
+            <Router>
                 <div className="mid-container">
-                    {this.state.loginForm ?
-                        <div className="loginForm">
-                            <form>
-                                <input placeholder="Username" name="username" required="" type="text" />
-                                <input placeholder="Password" name="password" required="" type="password" />
-                                <button type="button" onClick={this.login}>LOG IN</button>
-                            </form>
-                        </div>
-                        : null}
-                    {this.state.signupForm ?
-                        <div className="signupForm">
-                            <form>
-                                <input placeholder="Username" name="username" required="" type="text" />
-                                <input placeholder="Password" name="password" required="" type="password" />
-                                <button type="button" onClick={this.signup}>SIGN UP</button>
-                            </form>
-                        </div>
-                        : null}
+                    <Route exact path="/login" render={() => (
+                        <Login setCurrentUser={this.props.setCurrentUser} />
+                    )} />
+                    <Route exact path="/signup" render={() => (
+                        <Signup setCurrentUser={this.props.setCurrentUser} />
+                    )} />
                 </div>
                 <div className="c">
-                    <div className="pokeball" onClick={() => this.setState({ loginForm: true, signupForm: false })}>Login</div>
+                    <NavLink to="/login"><div className="pokeball">Login</div></NavLink>
                 </div>
                 <div className="d">
-                    <div className="pokeball" onClick={() => this.setState({ signupForm: true, loginForm: false })}>Sign up</div>
+                    <NavLink to="/signup"><div className="pokeball">Sign up</div></NavLink>
                 </div>
-            </>
+            </Router>
         );
     }
 }
